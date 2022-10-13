2e31ef884a338cc5a6f5f49dd44c8cc0bbb1a11f
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-10-13 01:35:03 +0100

    Printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,28 +1,38 @@
 #!/usr/bin/python3
+""" Printing a square"""
+
+
 class Square:
+    """Private instance attribute: size
+    Instantiation with area method prints squares """
+
     def __init__(self, size=0):
-        self.size = size
+        """Initializes attribute size """
+        self.__size = size
+
+    def area(self):
+        """Calculate area of square"""
+        return (self.__size * self.__size)
 
     @property
     def size(self):
+        """Setter for square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        """Initializes attribute size """
+        if (type(value) is not int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        a = self.__size * self.__size
-        return a
+        else:
+            self.__size = value
 
     def my_print(self):
-        if self.__size == 0:
-            print("")
-        for i in range(0, self.__size):
-            for j in range(0, self.__size):
-                print("#", end="")
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print('#', end="")
+            print()
+        if self.size <= 0:
             print()
