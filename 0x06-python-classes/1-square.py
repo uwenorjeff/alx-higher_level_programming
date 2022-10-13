4aab8deef690ea6e4849063de5f078ed4d2a368f
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-10-13 01:27:35 +0100

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""File defines a square"""
+
+
 class Square:
-    def __init__(self, size):
+    """A class Square that defines a square"""
+    def __init__(self, size=None):
+        """Initializes private attribute size """
         self.__size = size
