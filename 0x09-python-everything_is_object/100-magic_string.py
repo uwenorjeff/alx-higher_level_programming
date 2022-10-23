0aa85ddadee45adcbcc42600d72feba6363e41a8
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-10-23 06:29:37 +0100

    100-magic_string

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(H=[]):
-    H += ["Holberton"]
-    return (", ".join(H))
+def magic_string():
+    setattr(magic_string, "times", getattr(magic_string, "times", -1) + 1)
+    return "Best" + ", School" * getattr(magic_string, "times", 0)
