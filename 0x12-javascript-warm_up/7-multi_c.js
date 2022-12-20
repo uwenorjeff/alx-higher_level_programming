9518b08efef85cd62e3a2eee4b5d2f9996360bee
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-12-20 15:07:23 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 const newArgs = process.argv.slice(2);
 if (isNaN(newArgs[0]) === false) {
   const n = parseInt(newArgs[0]);
-  for (let i = 0; i < n; i++) { console.log('C is fun'); }
+  for (let i = 0; i < n; i++) { console.log('X'.repeat(n)); }
 } else {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
