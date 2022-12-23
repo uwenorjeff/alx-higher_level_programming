0c7afad337ead39284729edc68b93743afbb23aa
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-12-23 11:57:30 +0100

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-# Defines a State model.
-# Inherits from SQLAlchemy Base and links to the MySQL table states.
+'''
+Class definition of a State and an instance Base = declarative_base():
+'''
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from relationship_city import Base, City
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-        cities (sqlalchemy.orm.relationship): The State-City relationship.
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
+    '''
+    State class inherits from Base
+    '''
+    __tablename__ = 'states'
 
-    cities = relationship("City", backref="state", cascade="all, delete")
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete',
+                          backref='state')
