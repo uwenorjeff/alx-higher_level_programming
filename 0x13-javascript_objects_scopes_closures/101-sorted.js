697d68cbb215b465f2adab074934e9b755bae212
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-12-23 14:30:55 +0100

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-/* Task 101 - script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.  import dict from the file 101-data.js */
+
 const dict = require('./101-data').dict;
-const sort = (obj) => {
-  const reverse = {};
-  /* A key is a number of occurrences - A value is the list of user ids */
-  Object.keys(obj).forEach((key) => {
-    reverse[obj[key]] = reverse[obj[key]] || [];
-    reverse[obj[key]].push(key);
-  });
-  return reverse;
-};
-console.log(sort(dict));
+const output = {};
+
+for (const entry in dict) {
+  if (!output[dict[entry]]) output[dict[entry]] = [];
+  output[dict[entry]].push(entry);
+}
+
+console.log(output);
