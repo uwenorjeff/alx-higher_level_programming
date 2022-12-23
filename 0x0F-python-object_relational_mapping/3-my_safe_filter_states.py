2bdffbb835813c9fc50a34726e0d4f5b7db3e3f8
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-12-23 12:23:27 +0100

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-'''
-Take in argyment but make it s safe from MySQL injections.
-'''
+
+"""
+    A script that lists all states from the database hbtn_0e_0_usa
+    starting with capital letter N
+    Username, password and database names are given as user args
+"""
+
+
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
 
-        conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                               user=argv[1], passwd=argv[2], db=argv[3])
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states WHERE name=%s ORDER BY states.id",
-                    (argv[4], ))
-        query_rows = cur.fetchall()
-        for row in query_rows:
-                print(row)
-        cur.close()
-        conn.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    sql = """SELECT * FROM states
+          WHERE name = %s
+          ORDER BY id ASC"""
+
+    cursor.execute(sql, (sys.argv[4],))
+
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
