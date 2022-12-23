15942c90976fa33cb0113cc4537700c0a51a6022
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2022-12-23 12:24:34 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
-'''
-Lists all cities from the database hbtn_0e_4_usa.
-'''
 
+"""
+    A script that lists all cities in a state from the database hbtn_0e_0_usa
+    Username, password and database name and state are given as user args
+"""
+
+
+import sys
 import MySQLdb
-from sys import argv
 
-if __name__ == "__main__":
 
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                           user=argv[1], passwd=argv[2], db=argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
-    cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN\
-            states ON cities.state_id = states.id WHERE states.name=%s\
-            ORDER by cities.id ASC;",
-                (argv[4], ))
-    query_rows = cur.fetchall()
-    print(', '.join([row[1] for row in query_rows]))
-    cur.close()
-    conn.close()
+    cursor = db.cursor()
+
+    sql = """SELECT cities.name
+          FROM states
+          INNER JOIN cities ON states.id = cities.state_id
+          WHERE states.name = %s
+          ORDER BY cities.id ASC"""
+
+    cursor.execute(sql, (sys.argv[4],))
+
+    data = cursor.fetchall()
+
+    print(", ".join([city[0] for city in data]))
+
+    cursor.close()
+    db.close()
