3a973704e6289c34372230e802dacc17cb56ecd7
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2023-01-17 20:01:57 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""Check status"""
+"""
+Script that takes in a URL, sends a request to the URL and displays
+the body of the response.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+from sys import argv
 import requests
-import sys
 
-
-def errorcode():
-    """status"""
-    result = requests.get(sys.argv[1])
-    try:
-        if result.status_code > 400:
-            print("Error code: {}".format(result.status_code))
-        else:
-            print(result.content.decode("utf-8"))
-    except KeyError:
-        pass
 
 if __name__ == "__main__":
-    errorcode()
+    url = argv[1]
+    req = requests.get(url)
+
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
