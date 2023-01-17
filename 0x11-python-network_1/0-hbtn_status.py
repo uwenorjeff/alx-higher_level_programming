c1e38a8aaada1202c9e69edc99fe45180a66d211
uwenorjeff <106762251+uwenorjeff@users.noreply.github.com>
2023-01-17 19:06:49 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,11 @@
 import urllib.request
 
 
-def fetcher():
-    """fetcher"""
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-        html = response.read()
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        html = resp.read()
+
         print("Body response:")
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
-
-if __name__ == "__main__":
-    fetcher()
+        print("\t- utf8 content: {}".format(html.decode('utf8')))
